02c3b3fef12b13bcb70001a6f338ece2ad8598fc
Thamyres Cavalcante <thamyresmia@hotmail.com>
2022-11-12 15:43:27 -0300

    alterações

---
+++ TIPO-VARIAVEIS/src/operadores.java
@@ -0,0 +1,24 @@
+public class operadores {
+    public static void main(String[] args) {
+        String nomeComplet = "LINGUAGEM" + "JAVA";
+
+        String concatenacao = "?";
+        System.out.println( nome completo + concatenacao);
+
+        concatenacao = 1+1+1+"1";
+        System.out.println( concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+        System.out.println( concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+        System.out.println( concatenacao);
+
+        concatenacao = "1"+1+1+1;
+        System.out.println( concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+        System.out.println( concatenacao);
+
+    }
+}

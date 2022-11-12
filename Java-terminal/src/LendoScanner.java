3369196e217bd12a710134f6832f991f9cd1602b
Thamyres Cavalcante <thamyresmia@hotmail.com>
2022-11-12 20:45:45 -0300

    commit scanner

---
+++ Java-terminal/src/LendoScanner.java
@@ -0,0 +1,27 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class LendoScanner {
+    
+    public static void main(String[] args) {
+        
+        //criando o objeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);                 
+
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.next();
+        
+        System.out.println("Digite seu sobrenome: ");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite sua idade: ");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite sua altura: ");
+        double alura = scanner.nextDouble();
+
+        System.out.println("Ola, me chamo " + nome + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos ");
+        System.out.println("Minha altura eh " + alura +  " cm ");
+    }
+}

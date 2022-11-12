cf6c2ee6cdebde37874d2dbcf4c890270a2b0cfa
Thamyres Cavalcante <thamyresmia@hotmail.com>
2022-11-11 22:56:26 -0300

    atualização

---
+++ TIPO-VARIAVEIS/src/TiposVariaveis.java
@@ -0,0 +1,15 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        double salarioMinimo = 2500;
+        System.out.println(salarioMinimo);
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short)numeroNormal;
+
+        int numero = 5;
+        numero = 10;
+
+        System.out.println(numero);
+    }
+}

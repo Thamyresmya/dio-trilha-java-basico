ff7fe543fc599b16841be650636f89d7b142c599
Thamyres Cavalcante <thamyresmia@hotmail.com>
2022-11-13 11:16:45 -0300

    exceções

--- Java-terminal/src/LendoScanner.java
+++ Java-terminal/src/LendoScanner.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -5,9 +6,10 @@ public class LendoScanner {
     
     public static void main(String[] args) {
         
+        try{
         //criando o objeto scanner
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);                 
-
+        
         System.out.println("Digite seu nome: ");
         String nome = scanner.next();
         
@@ -19,9 +21,14 @@ public class LendoScanner {
 
         System.out.println("Digite sua altura: ");
         double alura = scanner.nextDouble();
-
+       
         System.out.println("Ola, me chamo " + nome + " " + sobrenome);
         System.out.println("Tenho " + idade + " anos ");
         System.out.println("Minha altura eh " + alura +  " cm ");
+
+        } 
+        catch (InputMismatchException e){
+            System.err.println("O Campo idade e altura, precisam ser numericos, e utilize o ponto");
+        }
     }
 }
